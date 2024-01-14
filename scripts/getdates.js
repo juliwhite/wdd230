28ee7df0ad3b2eeaac07de8b78b2d732ed4be502
juliwhite <julietawhite30@gmail.com>
2024-01-13 19:57:32 -0700

    css and js add

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+
+// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+
+// last modified property 
+let lastModif = document.lastModified;
+document.querySelector('#lastModified').textContent = lastModif;
