2b1d6941fae798f57b75f0f5be24ea3966879749
juliwhite <julietawhite30@gmail.com>
2024-01-24 21:54:40 -0700

    add hamburger menu

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,3 +6,22 @@ document.querySelector('#year').textContent = currentDate.getFullYear();
 // last modified property 
 let lastModif = document.lastModified;
 document.querySelector('#lastModified').textContent = lastModif;
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.nav-list')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
+
+
