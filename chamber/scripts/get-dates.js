15d985b7b5c7088bc586a359065afac89a6c3bb6
juliwhite <julietawhite30@gmail.com>
2024-02-10 12:46:46 -0700

    fix dark mode elements

--- chamber/scripts/get-dates.js
+++ chamber/scripts/get-dates.js
@@ -21,6 +21,42 @@ const body = document.querySelector("body");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-	body.classList.toggle('dark-mode');
+body.classList.toggle('dark-mode');
+main.classList.toggle('dark-mode');
+
+
+
+
+/* const header = document.querySelector("header");
+const links = document.querySelector("li a");
+const footer = document.querySelector("footer");
+const seconContainer = document.querySelector(".second-container");
+const spotlights = document.querySelector(".spotlights");
+const ev = document.querySelector(".event");
+const card = document.querySelector(".card");
+ */
+
+/* modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")){
+		header.style.background = "#000";
+		modeButton.textContent = "💡"
+	} else {
+		header.style.background = "#a8bd0a";
+		header.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	} */
+	
+	
+	
+	
+	
+	/*body.classList.toggle('dark-mode');
 	main.classList.toggle('dark-mode');
+	header.classList.toggle('dark-mode');
+	footer.classList.toggle('dark-mode');
+	seconContainer.classList.toggle('dark-mode');
+	spotlights.classList.toggle('dark-mode');
+	company.classList.toggle('dark-mode');
+	card.classList.toggle('dark-mode');
+	ev.classList.toggle('dark-mode');*/
 });
