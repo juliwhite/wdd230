899dc05b693d9332a800de2cb48713942f9af61a
juliwhite <julietawhite30@gmail.com>
2024-02-21 20:04:56 -0700

    add page visit counter (w07)

---
+++ lesson07/scripts/main.js
@@ -0,0 +1,89 @@
+// Declare three const variables that hold references to the 'input', 'button', and 'list' elements.
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+//Create a click event for the Add Chapter button using addEventListener.
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        //this.classList.add('error');
+        input.focus();
+    }
+    else {
+        this.classList.remove('error');
+    }
+
+    // Create an element li
+    const li = document.createElement('li');
+
+    // Create a delete button 
+    const deleteButton = document.createElement('button');
+
+    //Populate the li element textContent or innerHTML with the input value.
+    li.textContent = input.value;
+
+    // Populate the button textContent with 'X'.
+    deleteButton.textContent = '❌';
+
+    // Append the li element with the delete button.
+    li.append(deleteButton);
+
+    // Append the li element to the unordered list in HTML.
+    list.append(li);
+
+    // Add and event listener to the delete button that removes the li element when click. 
+    deleteButton.addEventListener('click', () => {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    // Send the focus to the input element.
+    input.focus();
+
+    // Change the input value to nothing or the empty string to clean up the interface for the user. 
+    input.value = '';
+})
+
+/**
+ Web Storage API - localStorage.
+ Week 7 - Use localStorage to store the BOM list for the user. 
+ */
+
+let bom_array = getListBofM() || [];
+
+// Populate the display list of chapter of Book of Mormom. 
+bom_array.forEach(chapter => {
+    display_list(chapter);
+});
+
+// Change de button click event in line 7. 
+button.addEventListener('click', () => {
+    if (input.value != '') {          // make sure the input is not empty.
+        display_list(input.value);    // call the function that outputs the submitted chapter.
+        bom_array.push(input.value);  // add the chapter to the array
+        setBomList();                 // update the localStorage with the new array
+        input.value = '',             // clear the input
+        input.focus();                // set the focus back to the input. 
+    }
+});
+
+// Create a function display_list with one parameter (item)
+function display_list(item) {
+    let li = document.createElement('li');
+    let delete_button = document.createElement('button');
+
+    li.textContent = item;        
+    delete_button.textContent = '❌';
+    delete_button.classList.add('delete');
+    li.append(delete_button);
+    list.append(li);
+    delete_button.addEventListener('click',() => {
+        list.removeChild(li);
+        delete_chapter(li.textContent);
+        input.focus();
+    });
+}
+
+function setBomList() {
+    localStorage.setItem('data', JSON.stringify(bom_array));
+}
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -24,4 +24,32 @@ The CSS class rules will handle the different views, layouts, and displays.
 🗝️ JavaScript only applies the class value or not.
 */
 
+/**
+ * Using localStorage Week 7
+ */
+
+const visitsDisplay = document.querySelector(".visits");
+
+// Get the stored VALUE	for the numVisitsLs KEY in localStorage if it exits. if the numVisits KEY is missing, then assign 0 to the numVisists variable.
+let numVisists = Number(window.localStorage.getItem("numVisistsLs")) || 0;
+
+// Determine if this is the first visit or display the number of visits. 
+if (numVisists !== 0) {
+	visitsDisplay.textContent = numVisists;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// Increment the number of visits by one.
+numVisists++;
+
+
+// Store the new visit total into localStorage, key = numVisitsLs
+localStorage.setItem("numVisistsLs", numVisists);
+
+
+
+
+
+// View localStorage data using the Application panel in the browser's DevTools. 
 
