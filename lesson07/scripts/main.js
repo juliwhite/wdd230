01f9ba78a8e0d98efb72035f76e793889876a635
juliwhite <julietawhite30@gmail.com>
2024-02-24 13:28:59 -0700

    add Book of Mormom localStorage

--- lesson07/scripts/main.js
+++ lesson07/scripts/main.js
@@ -4,7 +4,7 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 //Create a click event for the Add Chapter button using addEventListener.
-button.addEventListener('click', () => {
+/*button.addEventListener('click', () => {
     if (input.value != '') {
         //this.classList.add('error');
         input.focus();
@@ -42,14 +42,14 @@ button.addEventListener('click', () => {
 
     // Change the input value to nothing or the empty string to clean up the interface for the user. 
     input.value = '';
-})
+})*/
 
 /**
  Web Storage API - localStorage.
  Week 7 - Use localStorage to store the BOM list for the user. 
  */
 
-let bom_array = getListBofM() || [];
+let bom_array = getBomList() || [];
 
 // Populate the display list of chapter of Book of Mormom. 
 bom_array.forEach(chapter => {
@@ -79,11 +79,32 @@ function display_list(item) {
     list.append(li);
     delete_button.addEventListener('click',() => {
         list.removeChild(li);
-        delete_chapter(li.textContent);
+        deleteChapter(li.textContent);
         input.focus();
     });
 }
 
 function setBomList() {
     localStorage.setItem('data', JSON.stringify(bom_array));
+}
+
+// Define get function to get the local storage item. No parameter ir needed. this function returns to an awaiting array. JSON.parse will be used. 
+function getBomList() {
+    return JSON.parse(localStorage.getItem('data'));
+}
+
+/**
+ * The delete function with a parameter named chapter does 3 things:
+ * - reformat the chapter to ger rid of 'X' that is passed on the end of the 
+ * chapter string when the delete function is called. string.slice() extract the 
+ * last character. 
+ * - redefine the bom_array using the array.filter method to return everything
+ * except the chapter to be removed.
+ * - Call the setBomList function to update the localStorage.
+ */
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    bom_array = bom_array.filter(item => item !== chapter);
+    setBomList();
 }
