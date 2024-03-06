d99d8b3fc264630185f7a5bf902f085997afa92d
juliwhite <julietawhite30@gmail.com>
2024-03-06 14:10:13 -0700

    add JSON activity

--- scripts/links.js
+++ scripts/links.js
@@ -2,24 +2,54 @@ const baseURL = "https://juliwhite.github.io/wdd230/";
 
 const linksURL = "https://juliwhite.github.io/wdd230/data/links.json";
 
-//const lists = document.querySelector("#links")
+const lists = document.querySelector("#links")
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-    //displayLinks(data.lessons);
+    //console.log(data);
+    displayLinks(data.lessons);
   }
   
 getLinks();
 
 /*const displayLinks = (lessons) => {
-    lessons.forEach((week) => {
-        let list = document.createElement('li');
-        let a = document.createElement('a');
+    lessons.forEach((weeks) => {
 
-        list.textContent = `Week ${week.url} ${week.title}`;
+        //Create li element
+        const list = document.createElement('li');
 
+        //Create anchor tag.
+        const anchorTag = document.createElement('a');
+        
+
+        list.textContent = `Week ${weeks.lesson}: `; //;${week.links}`;
+       
+        anchorTag.href = `#${weeks.url} | ${weeks.title}`;
+
+        anchorTag.textContent = weeks;
+
+        //apend the anchor tag to the li.
+        list.appendChild(anchorTag);
+        
         lists.appendChild(list);
     });
 }*/
+
+const displayLinks = (lessons) => {
+    lessons.forEach((weeks) => {
+        const list = document.createElement('li');
+        list.textContent = `Week ${weeks.lesson}: `;
+
+        weeks.links.forEach(week => {
+            const anchorTag = document.createElement('a');
+            anchorTag.href = `${week.url}`;
+            anchorTag.textContent = `${week.title} `;
+
+            //append anchor to li
+            list.appendChild(anchorTag);      
+        });
+        //append the li to the lists
+        lists.appendChild(list);
+    });
+}
