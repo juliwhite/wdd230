075da7a9cb3083278546d9ad4eaae01f4fa50f57
juliwhite <julietawhite30@gmail.com>
2024-03-12 22:16:56 -0600

    add weather API activity (fix)

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -49,3 +49,41 @@ localStorage.setItem("numVisistsLs", numVisists);
 
 // View localStorage data using the Application panel in the browser's DevTools. 
 
+/**
+ * WEEK 10 - Weather API Activity
+ */
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+//43.492330447424315, -112.04675331438305
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.49&lon=-112.04&appid=46743c701fc291a73f9a2147e8db2a4f&units=imperial';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            //console.log(data); // testing
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    tem = Math.round(data.main.temp);
+    //currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    currentTemp.innerHTML = `${tem}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/10n.png`;
+    let desc = data.weather[0].description.toUpperCase();
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `weather icon`);
+    captionDesc.textContent = `${desc}`;
+}
